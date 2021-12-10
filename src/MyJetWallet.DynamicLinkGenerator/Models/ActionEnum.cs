5e1d8f52ae3a642f91b38b69afb374f28671a34f
Pavlo Pylypenko <85163968+pylypenko-smplt@users.noreply.github.com>
2021-12-10 13:02:02 +0200

    added action enum

---
+++ src/MyJetWallet.DynamicLinkGenerator/Models/ActionEnum.cs
@@ -0,0 +1,12 @@
+namespace Service.DynamicLinkGenerator.Domain.Models.Enums
+{
+    public enum ActionEnum
+    {
+        Login,
+        ConfirmEmail,
+        ForgotPassword,
+        ConfirmWithdrawal,
+        ConfirmTransfer,
+        InviteFriend
+    }
+}
