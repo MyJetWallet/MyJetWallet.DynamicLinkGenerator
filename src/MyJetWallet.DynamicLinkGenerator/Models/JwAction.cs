04ef574e3a4c1118b12951a34ba4d35310bfbf96
Pavlo Pylypenko <pavel.p@smplt.net>
2023-03-27 15:48:56 +0300

    new jw commants

--- src/MyJetWallet.DynamicLinkGenerator/Models/JwAction.cs
+++ src/MyJetWallet.DynamicLinkGenerator/Models/JwAction.cs
@@ -6,6 +6,10 @@ namespace Service.PushNotification.Domain.Models.Enums
     public enum JwAction
     {
        None,
-       jw_operation_history = 7
+       jw_operation_history = 7,
+       jw_kyc_documents_approved = 10,
+       jw_kyc_documents_declined = 11,
+       jw_kyc_banned = 12,
+
     }
 }
--- src/MyJetWallet.DynamicLinkGenerator/Services/ActionUrlClient.cs
+++ src/MyJetWallet.DynamicLinkGenerator/Services/ActionUrlClient.cs
@@ -8,4 +8,20 @@ public static class ActionUrlClient
     {
         return $"{baseUrl}action/{JwAction.jw_operation_history}/jw_operation_id/{operationId}";
     }
+    
+    public static string GenerateKycDocumentsApprovedUrl(string baseUrl)
+    {
+        return $"{baseUrl}action/{JwAction.jw_kyc_documents_approved}";
+    }
+
+    public static string GenerateKycDocumentsDeclinedUrl(string baseUrl)
+    {
+        return $"{baseUrl}action/{JwAction.jw_kyc_documents_declined}";
+    }
+
+    public static string GenerateKycBannedUrl(string baseUrl)
+    {
+        return $"{baseUrl}action/{JwAction.jw_kyc_banned}";
+    }
+
 }
